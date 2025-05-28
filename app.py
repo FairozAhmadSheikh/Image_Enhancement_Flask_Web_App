23e9d6f8c9c4341872e7bf92030ff2a13ead31ed
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-05-28 19:57:16 +0530

    Routes Defined

--- app.py
+++ app.py
@@ -12,3 +12,28 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        img = request.files["image"]
+        method = request.form["method"]
+
+        filename = secure_filename(img.filename)
+        input_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        img.save(input_path)
+
+        output_path = os.path.join(OUTPUT_FOLDER, f"output_{filename}")
+
+        if method == "esrgan":
+            enhance_esrgan(input_path, output_path)
+        elif method == "denoise":
+            denoise_image(input_path, output_path)
+        elif method == "clahe":
+            contrast_enhance(input_path, output_path)
+
+        return render_template("index.html", input_img=input_path, output_img=output_path)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
